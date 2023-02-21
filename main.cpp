a48c93bed00d4a91a6b5f71da1cf1b3b037619d2
Mason Mann <masonmann2020@gmail.com>
2023-02-21 13:25:02 -0800

    Quickly added user interaction

--- main.cpp
+++ main.cpp
@@ -23,6 +23,19 @@ int main(int argc, char* const argv[]) {
     emp_index.createFromFile("Employee.csv");
     
     // Loop to lookup IDs until user is ready to quit
+    bool done = false;
+
+    while (!done) {
+        int id;
+        cout << "Enter an ID to lookup (-1 to quit): ";
+        cin >> id;
+        if (id == -1) {
+            done = true;
+        } else {
+            Record record = emp_index.findRecordById(id);
+            record.print();
+        }
+    }
     
 
     return 0;
