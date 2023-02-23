4d9ac331b1566ac8c1715d02ab234f45ee48fb5b
LesherG <LesherG@oregonstate.edu>
2023-02-23 03:41:53 -0800

    Implemented basic addition to index, unfinished portions marked with TODO statements. Still need to add overflow block support, validity checking on extention and bit adding extension.

--- classes.h
+++ classes.h
@@ -32,6 +32,7 @@ class LinearHashIndex {
 private:
     const int BLOCK_SIZE = 4096;
     const int RECORD_SIZE = 716;
+    const float EXPAND_THRESH = .7;
 
     vector<int> blockDirectory; // Map the least-significant-bits of h(id) to a bucket location in EmployeeIndex (e.g., the jth bucket)
                                 // can scan to correct bucket using j*BLOCK_SIZE as offset (using seek function)
@@ -46,6 +47,8 @@ private:
     // BLOCK STRUCTURE
     // 1st byte is num records
     // 2nd byte is offset to overflow block
+    //  - e.g. offset of 1 flags next block as overflow
+    //  - offset of 0 indicates no overflow
     // Records start at 3rd byte
     //  - 5 records per block
     //  - 716 bytes each
@@ -63,19 +66,70 @@ private:
             LSBs -= (1 << (i - 1));
         }
 
-        // No records written to index yet
-        if (numRecords == 0) {
-            // Initialize index with first blocks (start with 4)
-
-        }
-
         // Add record to the index in the correct block, creating a overflow block if necessary
-        char block1[4096];
+        int block = blockDirectory.at(LSBs);
+        char blockData[BLOCK_SIZE];
+        FILE * index = fopen(fName.c_str(), "r+b");
 
+        fseek(index, BLOCK_SIZE * block, SEEK_SET);
+        fread(blockData, sizeof(char), BLOCK_SIZE, index);
+
+        // If the block is full, add overflow block
+        if(blockData[0] == 5){
+            // TODO: implement
+        } else {
+            writeRecord(record, block, blockData[0], index);
+        }
+        numRecords++;
+
+
+        // Check capacity
+        float total = 0;
+        for(int i = 0; i < n; i++){
+            fseek(index, BLOCK_SIZE * blockDirectory.at(i), SEEK_SET);
+            total += fgetc(index);
+            // TODO: make sure this doesn't miscount overflow blocks
+        }
+        float average = total / (n * 5);
         // Take neccessary steps if capacity is reached:
 		// increase n; increase i (if necessary); place records in the new bucket that may have been originally misplaced due to a bit flip
+        if(average >= EXPAND_THRESH){
+            blockDirectory.push_back(nextFreeBlock/BLOCK_SIZE);
+            // Write blank block to end of index
+            fseek(index, BLOCK_SIZE * nextFreeBlock + BLOCK_SIZE - 1, SEEK_SET);
+            fputc(0, index);
+
+            //TODO: check entries for if they're misplaced as per slides 61, 62 of indexing.pdf
 
 
+            // Update vars
+            n++;
+            nextFreeBlock += BLOCK_SIZE;
+
+            // if n > 2^i
+            //  add new bit
+            if(n > (1 << i)){
+                // TODO: implement
+            }
+            
+        }
+
+        fclose(index);
+    }
+
+    // write record into index file
+    void writeRecord(Record record, int blockNum, int recordNum, FILE * index){
+
+        fseek(index, BLOCK_SIZE * blockNum, SEEK_SET);
+        fputc(recordNum + 1, index);
+        // "1 +" to adjust for overflow offset var at second byte of block
+        fseek(index, 1 + recordNum * RECORD_SIZE, SEEK_CUR);
+        fputc(record.id, index);
+        fputc(record.manager_id, index);
+        // Write name and adjust file pointer based on length 
+        fputs(record.name.c_str(), index);
+        fseek(index, 200 - record.name.length(), SEEK_CUR);
+        fputs(record.bio.c_str(), index);
     }
 
 public:
